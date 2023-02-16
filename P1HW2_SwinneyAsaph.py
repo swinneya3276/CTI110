05ded8dead95c70ee481cb0ef4b440080142215b
swinneya3276 <124729093+swinneya3276@users.noreply.github.com>
2023-02-16 13:59:55 -0500

    Add files via upload

---
+++ P1HW2_SwinneyAsaph.py
@@ -0,0 +1,37 @@
+#This program calculates and displays travel expenses
+#2/16/2023
+#CTI-110 P1HW2
+#Asaph Swinney
+print("This progran calculates and displays travel expenses")
+print()
+#Declare variable and datatype 
+#Display ("Enter Budget: ")
+budget = float(input("Enter Budget: "))
+print()
+#Declare variable and datatype
+#Display ("Enter your travel destination: ")
+travelDestination = input("Enter your travel destination: ")
+print()
+#Declare variable and datatype
+#Display ("How much do you think you will spend on gas? ")
+gas = float(input("How much do you think you will spend on gas? "))
+print()
+
+hotel = float(input("Appoximately, how much will you need for accomodation/hotel? "))
+print()
+
+food = float(input("Last, how much do you need for food? "))
+print()
+
+print("-"*12,"Travel Expenses""-"*12)
+
+print("Location: ", travelDestination)
+print("Initial Budget: ", budget)
+print()
+print("Fuel: ", gas)
+print("Accomodation: ", hotel)
+print("Food: ", food)
+print()
+balance =  budget - (gas + hotel + food)
+print("Remaining Balance: ", balance )
+#print("Remaing Balnce ", format(balance,',0f'))
