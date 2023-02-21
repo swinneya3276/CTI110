3bd936de2b401ab20605d1f807bf7d55a313ecd9
swinneya3276 <124729093+swinneya3276@users.noreply.github.com>
2023-02-21 13:43:06 -0500

    Add files via upload

---
+++ P2LAB1_SwinneyAsaph.py
@@ -0,0 +1,7 @@
+mpg = float(input())
+cpg = float(input())
+cost_20 = (20 / mpg * cpg)
+cost_75 = (75 / mpg * cpg)
+cost_500 = (500 / mpg * cpg)
+
+print(f'{cost_20:.2f} {cost_75:.2f} {cost_500:.2f}')
