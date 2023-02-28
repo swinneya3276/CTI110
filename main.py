c60dc5c6ad569a90793c61d462c669b8b5734cda
swinneya3276 <124729093+swinneya3276@users.noreply.github.com>
2023-02-28 14:43:56 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+total = num1 * num2 * num3 * num4
+average = (num1 + num2 + num3 + num4) / 4 
+
+print(format(total,'.0f'), format(average,'.0f'))
+print(format(total,'.3f'), format(average,'.3f'))
