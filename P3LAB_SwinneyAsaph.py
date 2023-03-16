cfb583dc58c19d5b71a2fa34f98bf5f3cc1d73ba
swinneya3276 <124729093+swinneya3276@users.noreply.github.com>
2023-03-16 13:43:09 -0400

    Add files via upload

---
+++ P3LAB_SwinneyAsaph.py
@@ -0,0 +1,19 @@
+is_leap_year = False
+   
+input_year = int(input())
+
+if (input_year % 4) == 0:
+    if (input_year % 100) == 0:
+        if (input_year % 400) == 0:
+            is_leap_year = True
+        else:
+            is_leap_year = False
+    else:
+        is_leap_year = True
+  
+else:
+    is_leap_year = False
+if is_leap_year:
+    print(f'{input_year} - leap year')
+else:
+    print(f'{input_year} - not a leap year')
